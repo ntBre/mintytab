6eaaff03ef1db46b689e877a3f777f7d0792424b
Brent Westbrook <bwestbro@stedwards.edu>
2019-02-16 20:53:28 -0600

    moved to grid positioning instead of packing

--- brentab.py
+++ brentab.py
@@ -7,51 +7,56 @@ class App:
     def __init__(self, master):
         
         self.master = master
-        frame = Frame(master)
-        frame.pack()
-
-        self.button = Button(
-            frame, text="Quit", fg="red", command=frame.quit
-            )
-        self.button.pack(side=LEFT)
-
-        self.hi_there = Button(
-            frame, text="Hello", command=self.say_hi
-            )
-        self.hi_there.pack(side=LEFT)
 
         self.entries = []
-        self.i = 0
-        while self.i < 10:
-            self.entries.append(Entry(master))
-            self.entries[self.i].pack(side=LEFT)    
-            self.i+=1
+        self.i = 2
+        self.entry = self.i - 2
+        self.row_num = 1
+        self.col_num = 0
+        rows = 1
+        cols = 0
+        size = 5
 
+        while cols < size:
+            while rows < size + 1:
+                self.entries.append(Entry(master))
+                self.entries[self.entry].grid(row=rows, column=cols)
+                self.i+=1
+                self.entry = self.i - 2
+                self.row_num += 1
+                rows += 1
+            rows = 1
+            cols += 1
+            self.col_num += 1
+
+        f1 = Frame(self.master)
         self.add_row_button = Button(
-            frame, text="+", command=self.add_row
+            f1, text="+", command=self.add_row
             )
-        self.add_row_button.pack()
 
         self.sub_row_button = Button(
-            frame, text="-", command=self.sub_row
+            f1, text="-", command=self.sub_row
             )
-        self.sub_row_button.pack()
 
-    def say_hi(self):
-
-        print "hi there, everyone!"
+        f1.grid(row=0, column=0)
+        self.add_row_button.pack(side=LEFT)
+        self.sub_row_button.pack(side=LEFT)
 
     def add_row(self):
         
         self.entries.append(Entry(self.master))
         self.i += 1
-        self.entries[self.i-1].pack(side=LEFT)
+        self.entries[self.entry].grid(row=self.row_num)
+        self.entry = self.i - 2
+        self.row_num += 1
 
     def sub_row(self):
         
-        self.entries[self.i-1].pack_forget()
+        self.entries[self.entry-1].grid_forget()
         self.entries.pop()
         self.i -= 1
+        self.entry = self.i - 2
+        self.row_num += 1
         
 root = Tk()
 
